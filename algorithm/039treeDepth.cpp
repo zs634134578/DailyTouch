373c4520f0cf06a92cdecff718d50f90f2bedda6
suzhou <zhousu_95@163.com>
2014-08-28 16:23:57 +0800

    add isAVL

--- algorithm/039treeDepth.cpp
+++ algorithm/039treeDepth.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -34,6 +35,40 @@ public:
         return depthLeft >= depthRight ? depthLeft+1 : depthRight+1;
     }
 
+    bool isAVL(BinaryTreeNode* root)
+    {
+        if (root == NULL)
+            return true;
+        
+        int leftDepth = this->depth(root->m_pLeft);
+        int rightDepth = this->depth(root->m_pRight);
+        if (abs(leftDepth - rightDepth) <= 1)
+            return true && isAVL(root->m_pLeft) && isAVL(root->m_pRight);
+        else
+            return false;
+    }
+    bool isAVL(BinaryTreeNode* root, int* depth)
+    {
+        if (root == NULL)
+        {
+            *depth = 0;
+            return true;
+        }
+
+        int left = 0, right = 0;
+        if (this->isAVL(root->m_pLeft, &left)
+                && this->isAVL(root->m_pRight, &right))
+        {
+            if (abs(left - right) <= 1)
+            {
+                int tmp = (left>=right ? left : right);
+                *depth = 1 + tmp;                 
+                return true;
+            }
+        }
+        return false;
+    }
+
 };
 int main()
 {
@@ -50,6 +85,9 @@ int main()
 
     int depth = solver->depth(root);
     print(depth);
+    int tmpDepth = 0;
+    bool result = solver->isAVL(root, &tmpDepth);
+    print(result);
     print("Hello world!");
     return 0;
 }
