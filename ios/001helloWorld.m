f6f77a2f84ca3db39b818445dcb0ec50f3385af3
macAnswer <zhousu_95@163.com>
2014-11-24 12:59:02 +0800

    repair

--- ios/001helloWorld.m
+++
@@ -1,9 +0,0 @@
-#import
-
-int main(int argc, const char* argv[])
-{
-    NSAutoreleasePool * poll= [[NSAutoReleasePool alloc] init];
-    NSLog (@"hello world");
-    [pool drain];
-    return 0;
-}
