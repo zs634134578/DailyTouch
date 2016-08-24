b7a272770e2dcb68ad9da9e02e1e2fd0cb8fe4fd
suzhou <zhousu_95@163.com>
2016-08-25 00:14:34 +0800

    offers 005 need decide next root node

--- offers/005_ContructBianryTreeFromPreAndInOrder.cpp
+++ offers/005_ContructBianryTreeFromPreAndInOrder.cpp
@@ -10,6 +10,11 @@
  * };
  *
  **/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define DEBUG(key, value) std::cout << "[DEBUG]: " << __LINE__ << " " << key << " " << value << std::endl;
 
 struct TreeNode
 {
@@ -23,13 +28,64 @@ class Solution {
 
 public:
 
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder);
+    TreeNode* buildTree(std::vector<int>& preorder, std::vector<int>& inorder);
+	void PrintTreeInPreorder(TreeNode* tree);
+
+private:
+	TreeNode* constructCore(std::vector<int>& preorder,
+							int curRootIndexPreOrder,
+							std::vector<int>& inorder,
+							int startIndex, int endIndex);
 
 };
 
+TreeNode* Solution::constructCore(std::vector<int>& preorder,
+								  int curRootIndexPreOrder,
+								  std::vector<int>& inorder,
+								  int startIndex, int endIndex)
+{
+	int curRootValue = preorder[curRootIndexPreOrder];
+	TreeNode* curRootNode = new TreeNode(curRootValue);
+	if (startIndex == endIndex)
+	{
+		return curRootNode;
+	}
+	std::vector<int>::const_iterator it = std::find(inorder.begin() + startIndex, inorder.begin() + endIndex, curRootValue);
+	int curRootIndexInorder = it - inorder.begin(); 
+	if (startIndex < curRootIndexInorder)
+	{
+		curRootNode->left = constructCore(preorder, curRootIndexPreOrder + 1,
+				inorder, startIndex, curRootIndexInorder - 1);
+	}
+	if (endIndex > curRootIndexInorder)
+	{
+		curRootNode->right = constructCore(preorder, curRootIndexPreOrder + 1,
+				inorder, curRootIndexInorder + 1, endIndex);
+	}
+	return curRootNode;
+}
+
 TreeNode* Solution::buildTree(std::vector<int>& preorder, std::vector<int>& inorder)
 {
+	if (preorder.size() == 0
+		|| inorder.size() == 0
+		|| preorder.size() != inorder.size())
+	{
+		return NULL;
+	}
+	return constructCore(preorder, 0, inorder, 0, preorder.size() - 1);
 
+}
+
+void Solution::PrintTreeInPreorder(TreeNode* tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	std::cout << tree->val << " ";
+	PrintTreeInPreorder(tree->left);
+	PrintTreeInPreorder(tree->right);
 }
 
 
@@ -37,11 +93,13 @@ int main()
 {
 	int preorderArray[] = {1, 2, 4, 7, 3, 5, 6, 8};
 	int inorderArray[] = {4, 7, 2, 1, 5, 3, 8, 8};
-	std::vector<int>& preorder(preorderArray, preorderArray + sizeof(preorderArray)/sizeof(int));
-	std::vector<int>& inorder(inorderArray, inorderArray + sizeof(inorderArray)/sizeof(int));
+	std::vector<int> preorder(preorderArray, preorderArray + sizeof(preorderArray)/sizeof(int));
+	std::vector<int> inorder(inorderArray, inorderArray + sizeof(inorderArray)/sizeof(int));
 
 	Solution solution;
 	TreeNode* tree = solution.buildTree(preorder, inorder);
-
+	solution.PrintTreeInPreorder(tree);
+	std::cout << std::endl;
+	
 	return 0;
 }
